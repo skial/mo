8daa31463393394c7f1d25967d627ea59d2c7c94
Skial Bainn <skial.bainn@gmail.com>
2015-01-12 14:07:14 +0000

    Add support for namespaced attribute names.

--- src/uhx/lexer/HtmlLexer.hx
+++ src/uhx/lexer/HtmlLexer.hx
@@ -385,7 +385,7 @@ class HtmlLexer extends Lexer {
 	
 	public static var attributes = Mo.rules( [
 	'[ \r\n\t]' => lexer.token( attributes ),
-	'[a-zA-Z0-9_\\-]+[\r\n\t ]*=[\r\n\t ]*' => {
+	'[a-zA-Z0-9_\\-:]+[\r\n\t ]*=[\r\n\t ]*' => {
 		var index = lexer.current.indexOf('=');
 		var key = lexer.current.substring(0, index).rtrim();
 		var value = try {
