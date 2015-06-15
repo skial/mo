f5152688bf69ddef25c066e6d84da3c071ead9e9
Skial Bainn <skial.bainn@gmail.com>
2015-06-15 21:40:14 +0100

    Another blank slate.

---
+++ src/uhx/lexer/MimeLexer.hx
@@ -0,0 +1,14 @@
+package uhx.lexer;
+
+/**
+ * ...
+ * @author Skial Bainn
+ * @see https://en.wikipedia.org/wiki/Internet_media_type
+ */
+class MimeLexer {
+
+	public function new() {
+		
+	}
+	
+}
