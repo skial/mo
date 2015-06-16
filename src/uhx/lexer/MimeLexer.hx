c78a7634561f3d2a6cc3409c8f403f32823dbff6
Skial Bainn <skial.bainn@gmail.com>
2015-06-16 11:22:34 +0100

    Don't forget Subtype rule.

--- src/uhx/lexer/MimeLexer.hx
+++ src/uhx/lexer/MimeLexer.hx
@@ -84,6 +84,7 @@ class MimeLexer extends Lexer {
 	public static var root = Mo.rules( [
 	' \r\n\t' => lexer.token( root ),
 	'[a-zA-Z]+\\/' => Keyword( Toplevel( lexer.current.substring(0, lexer.current.length - 1).toLowerCase() ) ),
+	'[a-zA-Z]+' => Keyword( Subtype( lexer.current ) ),
 	'([a-zA-Z0-9\\-]+\\.?)+' => Keyword( Tree( lexer.current ) ),
 	'\\+[a-zA-Z0-9]+' => Keyword( Suffix( lexer.current.substring(1, lexer.current.length) ) ),
 	'; +[a-zA-Z0-9]+=[a-zA-Z0-9]+' => {
