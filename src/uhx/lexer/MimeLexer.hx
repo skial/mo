9722b9733f67f0fa61afaa077ff7c956ef90e3de
Skial Bainn <skial.bainn@gmail.com>
2015-06-16 11:25:23 +0100

    Correct whitespace rule.

--- src/uhx/lexer/MimeLexer.hx
+++ src/uhx/lexer/MimeLexer.hx
@@ -82,7 +82,7 @@ class MimeLexer extends Lexer {
 	}
 	
 	public static var root = Mo.rules( [
-	' \r\n\t' => lexer.token( root ),
+	'[ \r\n\t]*' => lexer.token( root ),
 	'[a-zA-Z]+\\/' => Keyword( Toplevel( lexer.current.substring(0, lexer.current.length - 1).toLowerCase() ) ),
 	'[a-zA-Z]+' => Keyword( Subtype( lexer.current ) ),
 	'([a-zA-Z0-9\\-]+\\.?)+' => Keyword( Tree( lexer.current ) ),
